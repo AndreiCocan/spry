8791e3bd0e9e4232a94f878df2163a2e24d207ad
AndreiCocan <95496161+AndreiCocan@users.noreply.github.com>
2024-04-06 17:45:35 +0200

    🐛Fixing linux path for brew

--- .chezmoiscripts/run_once_1_install_brew.sh
+++ .chezmoiscripts/run_once_1_install_brew.sh
@@ -13,11 +13,11 @@ export NONINTERACTIVE=1
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # shellcheck disable=SC2016
-
-{{ if eq .chezmoi.os "linux" }}
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-{{ end }}
+if test "$(uname -s)" == "Linux"; then
+  test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+  test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+fi
 
 brew analytics off
 echo "✅ [homebrew] Installed!"
